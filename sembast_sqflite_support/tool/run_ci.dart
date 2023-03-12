9f1be7839198594a8611600a05d23bf9a7ebe7a0
Alexandre Roux <alex@tekartik.com>
2023-03-12 19:04:09 +0100

    fix dart 3.0 tool

--- sembast_sqflite_support/tool/run_ci.dart
+++ sembast_sqflite_support/tool/run_ci.dart
@@ -1,8 +1,3 @@
-//
-// @dart = 2.9
-//
-// This is to allow running this file without null experiment
-// In the future, remove this 2.9 comment or run using: dart --enable-experiment=non-nullable --no-sound-null-safety run tool/travis.dart
 import 'dart:io';
 
 import 'package:dev_test/package.dart';
