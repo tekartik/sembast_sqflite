3493c7db0e18dbcf2743978e99bc795714f06c6f
Alexandre Roux <alex@tekartik.com>
2024-03-26 16:30:27 +0100

    deps update

--- sembast_sqflite_common_test/test/sembast_sqflite_common_ffi_test.dart
+++ sembast_sqflite_common_test/test/sembast_sqflite_common_ffi_test.dart
@@ -1,15 +1,17 @@
 @TestOn('vm')
 library sembast_sqflite_test.test.ffi_test;
 
+import 'package:sembast/sembast.dart';
 import 'package:sembast_sqflite/sembast_sqflite.dart';
 import 'package:sembast_test/all_jdb_test.dart' as all_jdb_test;
 import 'package:sembast_test/all_test.dart';
 import 'package:sembast_test/jdb_test_common.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-
+import 'package:path/path.dart';
 import 'ffi_setup.dart';
 
-var testPath = '.dart_tool/sembast_test/idb/databases';
+var testPath =
+    absolute(join('.dart_tool', 'sembast_sqflite_test', 'idb', 'databases'));
 
 Future main() async {
   await testSetup();
@@ -22,5 +24,29 @@ Future main() async {
   group('idb_io', () {
     defineTests(testContext);
     all_jdb_test.defineTests(testContext);
+  });
+
+  test('int vs String key', () async {
+    var intStore = intMapStoreFactory.store('int');
+    var stringStore = stringMapStoreFactory.store('string');
+    var dbPath = join(testPath, 'int_vs_string_key.db');
+    await factory.deleteDatabase(dbPath);
+    var db = await factory.openDatabase(dbPath);
+    try {
+      await intStore.add(db, {'value': 1});
+      await stringStore.add(db, {'value': 2});
+      await db.close();
+      db = await factory.openDatabase(dbPath);
+      var intRecords = await intStore.query().getSnapshots(db);
+      var stringRecords = await stringStore.query().getSnapshots(db);
+      print(intRecords);
+      print(stringRecords);
+      expect(intRecords.first.key, isA<int>());
+      expect(stringRecords.first.key, isA<String>());
+      intRecords = await intStore.query().onSnapshots(db).first;
+      stringRecords = await stringStore.query().onSnapshots(db).first;
+    } finally {
+      await db.close();
+    }
   });
 }
