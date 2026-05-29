9999e240d548dc5e1c789ba58ce077bf6ee0f3ac
Alexandre Roux <alex@tekartik.com>
2026-05-29 20:49:46 +0200

    chore: dart 3.12 update analysis options and linter rules

--- sembast_sqflite/lib/src/sql_constant.dart
+++ sembast_sqflite/lib/src/sql_constant.dart
@@ -5,21 +5,25 @@ const sqlKeyKey = dbRecordKey;
 
 /// The info table name
 const sqlInfoTable = 'info';
+
 /// The entry table name
 const sqlEntryTable = 'entry';
 
 /// The record index name
 const sqlRecordIndex = 'record';
+
 /// The deleted index name
 const sqlDeletedIndex = 'deleted';
 
 /// The primary key column name
 const sqlIdKey = 'id';
+
 /// The store column name
 const sqlStoreKey = dbStoreNameKey;
 
 /// Value encoded as a string.
 const sqlValueKey = dbRecordValueKey;
+
 /// The deleted column name
 const sqlDeletedKey = 'deleted';
 
